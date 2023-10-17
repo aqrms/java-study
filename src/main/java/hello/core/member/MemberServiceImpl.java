c66700b788552e291d8992f047a82c89f4c2965f
aqrms <aquarimus@gmail.com>
2023-10-18 01:59:17 +0900

    Study Sec03_03

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,15 +1,20 @@
 package hello.core.member;
 
-public class MemberServiceImpl implements MemberService{
+public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    @Override
-    public void join(Member member) {
-        memberRepository.save(member);
-    }
+	private final MemberRepository memberRepository;
 
-    @Override
-    public Member findMember(Long memberId) {
-        return memberRepository.findById(memberId);
-    }
+	public MemberServiceImpl(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
+
+	@Override
+	public void join(Member member) {
+		memberRepository.save(member);
+	}
+
+	@Override
+	public Member findMember(Long memberId) {
+		return memberRepository.findById(memberId);
+	}
 }
