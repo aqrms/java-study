00d365979ad2493f674eac705c104ec8f0bead71
aqrms <aquarimus@gmail.com>
2023-10-19 01:51:15 +0900

    Study Sec03 all

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,19 +1,37 @@
 package hello.core;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 
+@Configuration
 public class AppConfig {
 
-	public MemberService memberService(){
-		return new MemberServiceImpl(new MemoryMemberRepository());
+	@Bean
+	public MemberService memberService() {
+		return new MemberServiceImpl(MemberRepository());
 	}
 
-	public OrderService orderService(){
-		return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+	@Bean
+	public MemberRepository MemberRepository() {
+		return new MemoryMemberRepository();
+	}
+
+	@Bean
+	public OrderService orderService() {
+		return new OrderServiceImpl(MemberRepository(), DiscountPolicy());
+	}
+
+	@Bean
+	public DiscountPolicy DiscountPolicy() {
+		return new FixDiscountPolicy();
 	}
 }
